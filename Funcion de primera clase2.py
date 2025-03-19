1c7276284be9c305b58655f2e9050217762ea544
JacoboPat03 <jpatcoh@gmail.com>
2025-03-19 12:47:13 -0500

    Update Funcion de primera clase2.py

--- Funcion de primera clase2.py
+++ Funcion de primera clase2.py
@@ -1,4 +1,4 @@
-# Filtrar tareas pendientes de un proyecto
+# Filtrar tareas pendientes 
 tareas = [("Tarea 1: Exposicion", True), ("Tarea 2: Cuadro comparativo", True), ("Tarea 3:reporte de practicas", False)]
 tareas_pendientes = list(filter(lambda x: not x[1], tareas))
 print("Tareas pendientes:", tareas_pendientes)
